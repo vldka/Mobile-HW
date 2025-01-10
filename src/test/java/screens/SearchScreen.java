2e167b03889fd3a4e1e755b89d0c82d1398d13f6
Vlad Kashkarov <kwa-111@yandex.ru>
2025-01-11 02:58:43 +0300

    fix: увеличение количества тестов

--- src/test/java/screens/SearchScreen.java
+++ src/test/java/screens/SearchScreen.java
@@ -1,11 +1,18 @@
 package screens;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
+
+import com.codeborne.selenide.Selenide;
+
 import static io.appium.java_client.AppiumBy.*;
+import static org.assertj.core.api.Assertions.allOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SearchScreen {
@@ -30,6 +37,38 @@ public class SearchScreen {
         assertThat($$(id("org.wikipedia.alpha:id/page_list_item_title")))
                 .as("Expected at least one search result")
                 .hasSizeGreaterThan(0);
+    }
+
+    @Step("Проверка очистки поиска")
+    public static void checkHistorySearchResultsPresent(String text) {
+
+        $$(className("android.widget.TextView")).find(text(text)).should(exist);
+
+    }
+
+    @Step("Поиск со страницы по слову '{0}'")
+    public static void clickSearch(String searchText) {
+        $(id("org.wikipedia.alpha:id/page_toolbar_button_search")).click();
+        $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys(searchText);
+    }
+
+    @Step("Клик по поиску")
+    public static void clickSearch() {
+        $(id("org.wikipedia.alpha:id/page_toolbar_button_search")).click();
+    }
+
+    @Step("Очистить историю")
+    public static void clearSearch() {
+        $(id("org.wikipedia.alpha:id/recent_searches_delete_button")).click();
+        $(id("android:id/button1")).click();
+    }
+
+    @Step("Проверка очистки поиска")
+    public static void checkClearHistorySearchResultsPresent() {
+
+        $(id("org.wikipedia.alpha:id/search_empty_message"))
+                .shouldBe(text("Search Wikipedia in more languages"));
+
     }
 
     @Step("Проверка страницы '{0}'")
--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -9,22 +9,45 @@ import screens.SearchScreen;
 @DisplayName("Тест на проверку поиска")
 public class SearchTests extends TestBase {
     OnboardingScreen onboardingScreen = new OnboardingScreen();
+    String[] searchText = new String[]{"Main", "Java", "Sum"};
 
+        @Test
+        @DisplayName("Проверка поискового запроса")
+        void successfulSearchTest() {
+            onboardingScreen.clickSkipOnboardButton();
+            SearchScreen.performSearch(searchText[0]);
+            SearchScreen.verifySearchResultsPresent();
+        }
+        @Test
+        @DisplayName("Проверка Заголовка при переходе на статью")
+        void androidUnsuccessfulOpenTest() {
+            onboardingScreen.clickSkipOnboardButton();
+            SearchScreen.performSearch(searchText[0]);
+            SearchScreen.openFirstSearchResult();
+            SearchScreen.checkPageText(searchText[0]);
+        }
     @Test
-    @DisplayName("Проверка поискового запроса")
-    void successfulSearchTest() {
+    @DisplayName("Проверка истории поиска")
+    void checkHistorySearchTest() {
         onboardingScreen.clickSkipOnboardButton();
-        SearchScreen.performSearch("Appium");
-        SearchScreen.verifySearchResultsPresent();
+        SearchScreen.performSearch(searchText[0]);
+        SearchScreen.openFirstSearchResult();
+        SearchScreen.clickSearch();
+        SearchScreen.checkHistorySearchResultsPresent(searchText[0]);
     }
 
     @Test
-    @DisplayName("Проверка Заголовка при переходе на статью")
-    void androidUnsuccessfulOpenTest() {
-        String searchText = "Main";
+    @DisplayName("Проверка Очистки поиска")
+    void clearHistorySearchTest() {
         onboardingScreen.clickSkipOnboardButton();
-        SearchScreen.performSearch(searchText);
+        SearchScreen.performSearch(searchText[0]);
         SearchScreen.openFirstSearchResult();
-        SearchScreen.checkPageText(searchText);
+        SearchScreen.clickSearch(searchText[1]);
+        SearchScreen.openFirstSearchResult();
+        SearchScreen.clickSearch(searchText[2]);
+        SearchScreen.openFirstSearchResult();
+        SearchScreen.clickSearch();
+        SearchScreen.clearSearch();
+        SearchScreen.checkClearHistorySearchResultsPresent();
     }
 }
